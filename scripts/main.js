a95e36d5efa7b042f025627ae8d8f4da3bb742b5
Jennifer Mah <jen.mah@hotmail.com>
2015-05-12 12:24:08 +0100

    Have variables which store the players' moves and 'click' event listeners which show what has been played on the screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,71 @@
 console.log('this works!');
 
-var gameBoard = [[null,null,null],
-                 [null,null,null],
-                 [null,null,null]];
+
+// var gameBoard = [[null,null,null],
+//                  [null,null,null],
+//                  [null,null,null]];
+
+
+// move variables
+var firstMove = '';
+var secondMove = '';
+var thirdMove = '';
+var fourthMove = '';
+var fifthMove = '';
+var sixthMove = '';
+var seventhMove = '';
+var eigthMove = '';
+var ninthMove = '';
+
+
+// stores the players' moves
+var getMove = document.getElementsByClassName("button");
+
+  for(var i=0; i<getMove.length; i++) {
+    getMove[i].addEventListener('click', function(event) {
+      if (firstMove === '') {
+          console.log('first move made');
+          firstMove = parseInt(this.value);
+          // debugger;
+          $(this).html('X');
+      } else if (secondMove === '') {
+          console.log('second move made');
+          secondMove = parseInt(this.value);
+          $(this).html('O');
+      } else if (thirdMove === '') {
+          console.log('third move made');
+          thirdMove = parseInt(this.value);
+          $(this).html('X');
+      } else if (fourthMove === '') {
+          console.log('fourth move made');
+          fourthMove = parseInt(this.value);
+          $(this).html('O');
+      } else if (fifthMove === '') {
+          console.log('fifth move made');
+          fifthMove = parseInt(this.value);
+          $(this).html('X');
+      } else if (sixthMove === '') {
+          console.log('sixth move made');
+          sixthMove = parseInt(this.value);
+          $(this).html('O');
+      } else if (seventhMove === '') {
+          console.log('seventh move made');
+          seventhMove = parseInt(this.value);
+          $(this).html('X');
+      } else if (eigthMove === '') {
+          console.log('eigth move made');
+          eigthMove = parseInt(this.value);
+          $(this).html('O');
+      } else if (ninthMove === '') {
+          console.log('ninth move made');
+          ninthMove = parseInt(this.value);
+          $(this).html('X');
+      }
+    });
+  }
+
+
+
+
+
 
