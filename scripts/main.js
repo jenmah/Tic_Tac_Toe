1a17b9ee6d83af56ca0fb27a7e4379f58efbada0
Jennifer Mah <jen.mah@hotmail.com>
2015-05-12 18:03:32 +0100

    Created a variable with all of the possible winning combinations

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,36 @@
-var gameboard = [['null','null','null'],
-                 ['null','null','null'],
-                 ['null','null','null']];
+var gameboard = ['null','null','null','null','null','null','null','null','null'];
 
+var winningCombinations = ['X','X','X','null','null','null','null','null','null'];
+                          ['null','null','null','X','X','X','null','null','null'];
+                          ['null','null','null','null','null','null','X','X','X'];
+                          ['X','null','null','X','null','null','X','null','null'];
+                          ['null','X','null','null','X','null','null','X','null'];
+                          ['null','null','X','null','null','X','null','null','X'];
+                          ['X','null','null','null','X','null','null','null','X'];
+                          ['null','null','X','null','X','null','X','null','null'];
 
 var moveNumber = 0;
 
-// stores the players' moves
 $('.button').on('click', getMoves);
 function getMoves() {
-      if ((moveNumber % 2) === 0) {
-          console.log('first move made');
-          $(this).html('X');
-          currentMoveIndex = parseInt(this.value);
-          gameboard[$(this).data('position')][currentMoveIndex] = 'X';
-      } else {
-          console.log('second move made');
-          $(this).html('O');
-          currentMoveIndex = parseInt(this.value);
-          gameboard[$(this).data('position')][currentMoveIndex] = 'O';
-      }
-      moveNumber += 1;
-    } 
+    if ((moveNumber % 2) === 0) {
+        console.log('first player played');
+        $(this).html('X');
+        currentMoveIndex = parseInt(this.value);
+        gameboard[currentMoveIndex] = 'X';
+    } else {
+        console.log('second player played');
+        $(this).html('O');
+        currentMoveIndex = parseInt(this.value);
+        gameboard[currentMoveIndex] = 'O';
+    }
+    moveNumber += 1;
+} 
+
+
+function getWinner() {
+
+}
 
 
 
@@ -30,7 +40,19 @@ function getMoves() {
 
 
 
-
-
-
-
+// stores the players' moves
+// $('.button').on('click', getMoves);
+// function getMoves() {
+//       if ((moveNumber % 2) === 0) {
+//           console.log('first player played');
+//           $(this).html('X');
+//           currentMoveIndex = parseInt(this.value);
+//           gameboard[$(this).data('position')][currentMoveIndex] = 'X';
+//       } else {
+//           console.log('second player played');
+//           $(this).html('O');
+//           currentMoveIndex = parseInt(this.value);
+//           gameboard[$(this).data('position')][currentMoveIndex] = 'O';
+//       }
+//       moveNumber += 1;
+//     } 
