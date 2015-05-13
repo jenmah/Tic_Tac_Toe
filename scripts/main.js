49e4e76d94a716bdcc3ea16583d8c4b090c2bdda
Jennifer Mah <jen.mah@hotmail.com>
2015-05-13 16:14:05 +0100

    Have certain winning combinations declaring the winner

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,81 @@
-var gameboard = ['null','null','null','null','null','null','null','null','null'];
+var gameboard = ['0','1','2','3','4','5','6','7','8'];
+var newGameboard = [];
 
-var winningCombinations = ['X','X','X','null','null','null','null','null','null'];
-                          ['null','null','null','X','X','X','null','null','null'];
-                          ['null','null','null','null','null','null','X','X','X'];
-                          ['X','null','null','X','null','null','X','null','null'];
-                          ['null','X','null','null','X','null','null','X','null'];
-                          ['null','null','X','null','null','X','null','null','X'];
-                          ['X','null','null','null','X','null','null','null','X'];
-                          ['null','null','X','null','X','null','X','null','null'];
+var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+// var winningCombinations = [0, 1, 2];
 
 var moveNumber = 0;
+
+var xPlayerIndex = [];
+var yPlayerIndex = [];
+
 
 $('.button').on('click', getMoves);
 function getMoves() {
     if ((moveNumber % 2) === 0) {
-        console.log('first player played');
-        $(this).html('X');
-        currentMoveIndex = parseInt(this.value);
-        gameboard[currentMoveIndex] = 'X';
-    } else {
-        console.log('second player played');
-        $(this).html('O');
-        currentMoveIndex = parseInt(this.value);
-        gameboard[currentMoveIndex] = 'O';
+      console.log('first player played');
+      $(this).html('X');
+      currentMoveIndex = parseInt(this.value);
+      gameboard[currentMoveIndex] = 'X';
+      newGameboard = gameboard;
+      xPlayerIndex.push(currentMoveIndex);
+        for(var x = 0; x < xPlayerIndex.length; x++) {
+          for(var y = 0; y < winningCombinations.length; y++) {
+            if(xPlayerIndex.toString() ==  winningCombinations[y].toString()) {
+            $('h2').html('PLAYER ONE WON!');
+            }
+          }
+        }
+  } else {
+      console.log('second player played');
+      $(this).html('O');
+      currentMoveIndex = parseInt(this.value);
+      gameboard[currentMoveIndex] = 'O';
+      newGameboard = gameboard;
+      yPlayerIndex.push(currentMoveIndex);
+        for(var x = 0; x < yPlayerIndex.length; x++) {
+          for(var y = 0; y < winningCombinations.length; y++) {
+            if(yPlayerIndex.toString() ===  winningCombinations[y].toString()) {
+            $('h2').html('PLAYER TWO WON!');
+            }
+          }
+        }
     }
     moveNumber += 1;
 } 
 
 
-function getWinner() {
-
-}
-
-
-
+$('.refresh-button').on('click', reload);
+    function reload() {
+      window.location.reload();
+  }
 
 
+// function playToFive() {
+//     console.log("Let's play Rock, Paper, Scissors");
+//     var playerWins = 0;
+//     var computerWins = 0;
+    
+//     while (playerWins < 5 && computerWins < 5)  {
+//     var playerMove = getPlayerMove();
+//     var computerMove = getComputerMove();
+//     var winner = getWinner(playerMove,computerMove);
+    
+//     if (winner === 'player') {
+//             playerWins += 1;
+//         } else if (winner === 'computer') {
+//             computerWins += 1;
+//         }
 
-
-
-
-// stores the players' moves
-// $('.button').on('click', getMoves);
-// function getMoves() {
-//       if ((moveNumber % 2) === 0) {
-//           console.log('first player played');
-//           $(this).html('X');
-//           currentMoveIndex = parseInt(this.value);
-//           gameboard[$(this).data('position')][currentMoveIndex] = 'X';
-//       } else {
-//           console.log('second player played');
-//           $(this).html('O');
-//           currentMoveIndex = parseInt(this.value);
-//           gameboard[$(this).data('position')][currentMoveIndex] = 'O';
-//       }
-//       moveNumber += 1;
-//     } 
+//   console.log('Player used ' + playerMove + ' and the Computer used ' + computerMove);
+//   console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+//     }
+//     if (playerWins === 5) {
+//         console.log("Player wins the game!");
+//     } else {
+//         console.log("Computer wins the game!");
+//         return [playerWins, computerWins];
+//     }
+// }
+// playToFive();
