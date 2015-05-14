b1acac89ccdf8845e5e9fd4d8267de7ca374ae04
Jennifer Mah <jen.mah@hotmail.com>
2015-05-14 15:28:46 +0100

    Made major CSS for the gameboard and navigaton bar

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,13 @@ var newGameboard = [];
 var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 var moveNumber = 0;
-
 var xPlayerIndex = [];
 var yPlayerIndex = [];
 var firstPlayerWins = 0;
 var secondPlayerWins = 0;
 var winner = '';
 var nameOne = '';
+var nameTwo = '';
 
 $('.button').on('click', getMoves);
 function getMoves() {
@@ -38,25 +38,27 @@ function getMoves() {
       yPlayerIndex.push(currentMoveIndex);
         $.each(winningCombinations, function(index, value) {
           if ( $.inArray(value[0], yPlayerIndex) !== -1 && $.inArray(value[1], yPlayerIndex) !== -1 && $.inArray(value[2], yPlayerIndex) !== -1) {
-              $('h2').html('PLAYER TWO WON!');
+              $('h2').html(nameTwo.toUpperCase() + ' WON THIS GAME!');
               secondPlayerWins += 1;
               // winner = secondPlayer;
           }
         })
-    } else if (xPlayerIndex.length === 5) {
+    } else if (moveNumber === 9) {
         $('h2').html('IT IS A DRAW!');
       }   
     moveNumber += 1;
 } 
 
-
+// Collect names from the first and second player so they appear on the scoreboard
 $('.name-button').on('click', function(event) {
-  nameOne = $('#new-item').val();
+  nameOne = $('#player-one-name').val();
   console.log(nameOne);
+  nameTwo = $('#player-two-name').val();
+  console.log(nameTwo);
 });
 
 
-
+// Page refreshes and board clears when refresh button is clicked
 $('.refresh-button').on('click', reload);
     function reload() {
       window.location.reload();
